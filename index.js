7ab9307c775ab69db7921d8aebc2bde618861bf7
ItsSamich <alexanderklein@mail.weber.edu>
2020-10-19 01:01:14 -0600

    Reverted Changes for BruteForce Method

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
 var board = [];
 var cells = document.getElementsByClassName('cell'); //defines an array to hold the cell values
-var counter = 0;
-
-//Function that starts the program
-function solve(){
-    var start = 0;
-    var end = 0;
 
 
-    if(counter < 17){
-        alert("Not a valid input. No Unique Solution");
-        resetBoard();
-        return;
-    }
-
-    getBoard(); //Sets up the board with user defined values
-    
-    
-    start = performance.now(); //the start timer for runtime
-    sudokuSolver(board); //runs solver function
-    var end = performance.now(); //the end timer for runtime
-    if(sudokuSolver(board)){
-        document.getElementById('timer').innerHTML = "Time taken to solve with Backtracking: " + (end - start) + " ms"; //updates the timer div in index.html to display backtracking time
-        updateBoard(); //updates board with all correct values found from the solver
-    }
-    else{
-        alert("There is no solution!"); //if no valid solution, alerts the user of it.
-    }
-
-}
-
-
-
-//Handles board and input management
 function getBoard(){
     Board(); //Creates a new board
     for(let i = 0; i< 9; i++){
@@ -40,17 +9,9 @@ function getBoard(){
             index = i * 9 + j; //Sets the postition the board is at in the array
             cell = cells[index]; //finds the cell at the position board is at in array of cells
             board[i][j] = cell.value; //takes the cell value and puts it in a 2D array for board position
-            validValue(cell.value); //makes sure that the value is valid
-            console.log(counter);
         }
     }
 
-
-}
-function validValue(value){
-    if(value != ''){
-        counter++;
-    }
 }
 function Board(){
     board = [
@@ -65,6 +26,7 @@ function Board(){
         [0,0,0,0,0,0,0,0,0]
     ];
 }
+
 function updateBoard(){
     for(let i = 0; i < 9; i++){
         for(let j = 0; j < 9; j++){
@@ -79,18 +41,34 @@ function resetBoard(){
 }
 
 
+function solve(){
+    var start = 0;
+    var end = 0;
+    getBoard(); //Sets up the board with user defined values
+    start = performance.now(); //the start timer for runtime
+    sudokuSolver(board); //runs solver function
+    var end = performance.now(); //the end timer for runtime
+    if(sudokuSolver(board)){
+        document.getElementById('timer').innerHTML = "Time taken to solve with Backtracking: " + (end - start) + " ms"; //updates the timer div in index.html to display backtracking time
+        updateBoard(); //updates board with all correct values found from the solver
+    }
+    else{
+        alert("There is no solution!"); //if no valid solution, alerts the user of it.
+    }
 
-//Backtracking Algorithm
+}
+
 function isValid(board, row, col, value){
     for(let i = 0; i < 9; i++){
         var m = 3 * Math.floor(row / 3) + Math.floor(i/3);
         var n = 3 * Math.floor(col/3) + i % 3;
-        if(board[row][i] === value || board[i][col] === value || board[m][n] === value){
+        if(board[row][i] == value || board[i][col] == value || board[m][n] == value){
             return false;
         }
     }
     return true;
 }
+
 function sudokuSolver(board){
     for(let i = 0; i < 9; i++){ //Iterates for rows
         for(let j = 0; j < 9; j++){ //Iterates for columns
