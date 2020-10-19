2c47401870326d014e3715125f9003426eac08ab
ItsSamich <alexanderklein@mail.weber.edu>
2020-10-19 02:48:30 -0600

    Added import+ solve from string option

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var board = [];
+var board;
 var cells = document.getElementsByClassName('cell'); //defines an array to hold the cell values
-var counter = 17;
+var counter = 0; //counter to check if minimum hint count is met(17)
 
 
 function getBoard(){
@@ -36,6 +36,9 @@ function updateBoard(){
         for(let j = 0; j < 9; j++){
             index = i*9+j; //Sets the postition the board is at in the array
             cell = cells[index]; //finds the cell at the position board is at in array of cells
+            if(board[i][j] == "0"){ //checks if value at position is "0"
+                board[i][j] = ''; //converts a "0" value to a empty character value to allow for proper board display and solving
+            }
             cell.value = board[i][j]; //takes the value at the board position and returns it to the cell
         }
     }
@@ -46,13 +49,13 @@ function resetBoard(){
 
 
 function solve(){
-    var start = 0;
-    var end = 0;
+    var start = 0; //start of runtime timer
+    var end = 0; //end of runtime timer
 
-    if(counter < 17){
-        alert("Not a valid input. No Unique Solution");
-        resetBoard();
-        return;
+    if(counter < 17){ //checks the counter value to see if it is at the minimum
+        alert("Not a valid input. No Unique Solution"); //alerts the user
+        resetBoard(); //resets the board so no value remains
+        return; //ends the program
     }
 
     getBoard(); //Sets up the board with user defined values
@@ -70,14 +73,14 @@ function solve(){
 }
 
 function isValid(board, row, col, value){
-    for(let i = 0; i < 9; i++){
-        var m = 3 * Math.floor(row / 3) + Math.floor(i/3);
-        var n = 3 * Math.floor(col/3) + i % 3;
-        if(board[row][i] == value || board[i][col] == value || board[m][n] == value){
-            return false;
+    for(let i = 0; i < 9; i++){ //position
+        var m = 3 * Math.floor(row / 3) + Math.floor(i/3); //row checker for the 3x3 box
+        var n = 3 * Math.floor(col/3) + i % 3; //column checker for the 3x3 box
+        if(board[row][i] == value || board[i][col] == value || board[m][n] == value){ //checks if value is found in row, column, and a 3x3 box respectively
+            return false; //returns false if found
         }
     }
-    return true;
+    return true; //returns true if not found
 }
 
 function sudokuSolver(board){
@@ -100,4 +103,21 @@ function sudokuSolver(board){
         }
     }
     return true;
+}
+
+function importPuzzle(){
+    var importedValue = document.getElementById("import").value //saves the imported value in a variable
+    if(importedValue.length != 81){ //checks that the imported string is 81 characters long to fit perfectly in board
+        alert("Please enter a valid puzzle"); //alerts user
+    }
+    else{
+        Board(); //generates a new board;
+        counter = 81; //sets the counter to 81 to allow for solving
+        for(let i = 0; i < 9; i++){ //row
+            for(let j = 0; j < 9; j++){ //column
+                board[i][j] = importedValue.charAt(i*9+j); //sets the value at the given position in the board
+            }
+        }
+        updateBoard(); //updates and draws the board
+    }
 }
